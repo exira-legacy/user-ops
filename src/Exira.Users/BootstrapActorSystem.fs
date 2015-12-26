e3c3b11092e84df2b55e61a8199b7fcb3438ad31
David Cumps <david@cumps.be>
2015-12-26 21:02:23 +0100

    Reorganise files

---
+++ src/Exira.Users/BootstrapActorSystem.fs
@@ -0,0 +1,24 @@
+﻿namespace Exira.Users
+
+[<AutoOpen>]
+module internal BootstrapActorSystem =
+    open Akka.FSharp
+    open Akka.FSharp.Spawn
+    open Topshelf.FSharpApi.Time
+
+    let private configuration = Settings.configuration
+
+    type private Message = { Text: string }
+
+    let private dummyActor message =
+        printfn "Got message: %s" message.Text
+
+    let bootstrap () =
+        let system = System.create configuration.Service.ServiceName (Configuration.load())
+
+        let dummyActor = spawn system "dummy" (actorOf dummyActor)
+
+        let hiMessage = { Text = "Hello!" }
+        system.Scheduler.ScheduleTellRepeatedly(s 5, s 1, dummyActor, hiMessage)
+
+        Some system
--- src/Exira.Users/Program.fs
+++ src/Exira.Users/Program.fs
@@ -1,56 +1,37 @@
-﻿open System.Reflection
-open System.IO
-open FSharp.Configuration
-open Topshelf
-open Time
-open Akka.FSharp
-open Akka.FSharp.Spawn
-open Akka.Actor
+﻿namespace Exira.Users
 
-let executablePath = Assembly.GetEntryAssembly().Location |> Path.GetDirectoryName
-let configPath = Path.Combine(executablePath, "Configuration.yaml")
+module Program =
+    open Topshelf
+    open Time
+    open Akka.Actor
 
-type Configuration = YamlConfig<"Configuration.yaml">
-let configuration = Configuration()
-configuration.Load configPath
+    let private configuration = Settings.configuration
 
-let mutable (actorSystem: ActorSystem option) = None
+    let mutable (actorSystem: ActorSystem option) = None
 
-type Message = { Text: string }
+    let stop _ =
+        match actorSystem with
+            | None -> true
+            | Some system ->
+                system.Shutdown()
+                system.AwaitTermination()
+                actorSystem <- None
+                true
 
-let dummyActor message =
-    printfn "Got message: %s" message.Text
+    let start _ =
+        actorSystem <- bootstrap()
+        true
 
-let stop _ =
-    match actorSystem with
-        | None -> true
-        | Some system ->
-            system.Shutdown()
-            system.AwaitTermination()
-            actorSystem <- None
-            true
-
-let start _ =
-    let system = System.create configuration.Service.ServiceName (Configuration.load())
-
-    let dummyActor = spawn system "dummy" (actorOf dummyActor)
-
-    let hiMessage = { Text = "Hello!" }
-    system.Scheduler.ScheduleTellRepeatedly(s 5, s 1, dummyActor, hiMessage)
-
-    actorSystem <- Some system
-    true
-
-[<EntryPoint>]
-let main _ =
-    Service.Default
-    |> run_as_local_system
-    |> start_auto
-    |> enable_shutdown
-    |> with_recovery (ServiceRecovery.Default |> restart (min configuration.Service.RestartIntervalInMinutes))
-    |> with_start start
-    |> with_stop stop
-    |> description configuration.Service.Description
-    |> display_name configuration.Service.ServiceName
-    |> service_name configuration.Service.ServiceName
-    |> run
+    [<EntryPoint>]
+    let main _ =
+        Service.Default
+        |> run_as_local_system
+        |> start_auto
+        |> enable_shutdown
+        |> with_recovery (ServiceRecovery.Default |> restart (min configuration.Service.RestartIntervalInMinutes))
+        |> with_start start
+        |> with_stop stop
+        |> description configuration.Service.Description
+        |> display_name configuration.Service.ServiceName
+        |> service_name configuration.Service.ServiceName
+        |> run
