41eb289402e66aec10fd7facdd04ea185f4d8aba
David Cumps <david@cumps.be>
2015-12-27 01:11:40 +0100

    Add web project

---
+++ src/Exira.Users.Web/Settings.fs
@@ -0,0 +1,13 @@
+﻿namespace Exira.Users.Web
+
+module internal Settings =
+    open System.Reflection
+    open System.IO
+    open FSharp.Configuration
+
+    let private executablePath = Assembly.GetEntryAssembly().Location |> Path.GetDirectoryName
+    let private configPath = Path.Combine(executablePath, "Configuration.yaml")
+
+    type Configuration = YamlConfig<"Configuration.yaml">
+    let configuration = Configuration()
+    configuration.LoadAndWatch configPath |> ignore
