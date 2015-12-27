ef367286722bde8ec9332bdc852fc75eb01f00dc
David Cumps <david@cumps.be>
2015-12-27 03:51:21 +0100

    Play with Identify

--- src/Exira.Users.Web/ActorSystem.fs
+++ src/Exira.Users.Web/ActorSystem.fs
@@ -4,18 +4,25 @@
 module internal ActorSystem =
     open System
     open Akka.FSharp
+    open Akka.Actor
 
     let private configuration = Settings.configuration
 
     let bootstrapActorSystem () =
         let system = System.create configuration.Akka.Name (Configuration.load())
 
-        let dummyActor =
-            system
-                .ActorSelection("akka.tcp://user-ops@127.0.0.1:8901/user/dummy")
-                .ResolveOne(TimeSpan.FromSeconds 3.)
-                .Result
+        async {
+            let dummyActorPath = select "akka.tcp://user-ops@127.0.0.1:8901/user/dummy" system
 
-        system.Scheduler.ScheduleTellRepeatedly(TimeSpan.FromSeconds 5., TimeSpan.FromSeconds 1., dummyActor, "hello")
+            let! reply = dummyActorPath <? Identify("correlation-id")
+            let dummyActor =
+                match (reply: obj) with
+                | :? ActorIdentity as identity when not(isNull identity.Subject) -> Some identity.Subject
+                | _ -> None
+
+            match dummyActor with
+            | Some a ->  system.Scheduler.ScheduleTellRepeatedly(TimeSpan.FromSeconds 5., TimeSpan.FromSeconds 1., a, "hello")
+            | None -> ()
+        } |> Async.RunSynchronously
 
         Some system
