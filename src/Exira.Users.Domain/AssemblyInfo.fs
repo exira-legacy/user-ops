784f4af027c727f0154f3e086a4473c6d81cfbfe
David Cumps <david@cumps.be>
2015-12-28 16:06:31 +0100

    Simplify dependencies

--- src/Exira.Users.Domain/AssemblyInfo.fs
+++ src/Exira.Users.Domain/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Exira.Users.Domain")>]
 [<assembly: AssemblyProductAttribute("Exira.Users")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.Users is an event sourced microservice to manage users.")>]
-[<assembly: AssemblyVersionAttribute("0.4.18")>]
-[<assembly: AssemblyFileVersionAttribute("0.4.18")>]
-[<assembly: AssemblyMetadataAttribute("githash","c7ae56d3a20c5b770487214dbf24936ccf15022a")>]
+[<assembly: AssemblyVersionAttribute("0.4.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.4.0")>]
+[<assembly: AssemblyMetadataAttribute("githash","08ff4ad1d3a9c2b70b4cff61e02086e54a698811")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.4.18"
+    let [<Literal>] Version = "0.4.0"
