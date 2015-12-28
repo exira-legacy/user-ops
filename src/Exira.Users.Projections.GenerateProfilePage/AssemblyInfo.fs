154ca97d36b574d3915cfc2e515b3c2eecc87aec
Team City <info@exira.com>
2015-12-28 20:18:55 +0000

    Bump version to 0.4.20

--- src/Exira.Users.Projections.GenerateProfilePage/AssemblyInfo.fs
+++ src/Exira.Users.Projections.GenerateProfilePage/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Exira.Users.Projections.GenerateProfilePage")>]
 [<assembly: AssemblyProductAttribute("Exira.Users")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.Users is an event sourced microservice to manage users.")>]
-[<assembly: AssemblyVersionAttribute("0.4.19")>]
-[<assembly: AssemblyFileVersionAttribute("0.4.19")>]
-[<assembly: AssemblyMetadataAttribute("githash","784f4af027c727f0154f3e086a4473c6d81cfbfe")>]
+[<assembly: AssemblyVersionAttribute("0.4.20")>]
+[<assembly: AssemblyFileVersionAttribute("0.4.20")>]
+[<assembly: AssemblyMetadataAttribute("githash","1732e8f4561ee6a13cd442dde61e77c692748c21")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.4.19"
+    let [<Literal>] Version = "0.4.20"
