a80a51840627cfda053b4ba268999a3176da0312
David Cumps <david@cumps.be>
2016-01-15 23:57:49 +0100

    First go at property based testing

---
+++ tests/Exira.Users.Domain.Tests/AccountProperties.fs
@@ -0,0 +1,9 @@
+﻿module Exira.Users.Domain.Tests.AccountProperties
+
+open FsCheck.Xunit
+open Exira.Users.Domain.Commands
+
+[<Property>]
+let ``when creating an account`` (cmd: CreateAccountCommand) =
+    printfn "%A" cmd
+    true
