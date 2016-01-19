98789c22fb9dbcec6f79028b6f039e8bccb4aa9c
David Cumps <david@cumps.be>
2016-01-19 21:01:22 +0100

    Static linking problem

---
+++ src/Exira.Serilog/TypeTagRemover.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Exira.Serilog
+{
+    class TypeTagRemover : ILogEventEnricher
+    {
+        public void Enrich(LogEvent logEvent, ILogEventPropertyFactory factory)
+        {
+            // The ToArray() might be avoidable...
+            foreach (var prop in logEvent.Properties.ToArray())
+            {
+                var val = prop.Value as StructureValue;
+                if (val?.TypeTag != null)
+                {
+                    var replacement = new LogEventProperty(prop.Key, new StructureValue(val.Properties));
+                    logEvent.AddOrUpdateProperty(replacement);
+                }
+            }
+        }
+    }
+
+}
--- src/Exira.Users.Domain/AssemblyInfo.fs
+++ src/Exira.Users.Domain/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Exira.Users.Domain")>]
 [<assembly: AssemblyProductAttribute("Exira.Users")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.Users is an event sourced microservice to manage users.")>]
-[<assembly: AssemblyVersionAttribute("0.5.53")>]
-[<assembly: AssemblyFileVersionAttribute("0.5.53")>]
-[<assembly: AssemblyMetadataAttribute("githash","1452d62578b9de59677f2593817ae42a1523db11")>]
+[<assembly: AssemblyVersionAttribute("0.5.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.5.0")>]
+[<assembly: AssemblyMetadataAttribute("githash","1213ddf0d99acade6a06764edbbbddf04c4b25cb")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.5.53"
+    let [<Literal>] Version = "0.5.0"
